293be656c08c260f23a56a5955233bfc81b72b71
Arianna Cagali <ariannacagali@Ariannas-MacBook-Air.local>
2024-04-14 15:54:53 +0200

    Added .bin loader (to be checked)

---
+++ include/PlockY/BinBlockLoaderFactory.hpp
@@ -0,0 +1,12 @@
+//
+//  BinBlockLoaderFactory.hpp
+//  
+//
+//  Created by Arianna Cagali on 14/04/24.
+//
+
+#ifndef BinBlockLoaderFactory_h
+#define BinBlockLoaderFactory_h
+
+
+#endif /* BinBlockLoaderFactory_h */
